941c70e5018ef5bb9f8a1e21509904b2fd19b183
edward01s2 <edward.01s2@gmail.com>
2019-02-11 13:36:53 -0600

    Added button to second stream

--- src/js/site.js
+++ src/js/site.js
@@ -1,4 +1,12 @@
-jQuery( document ).ready( function( $ ) {
+$( document ).ready( function( $ ) {
   // Your JavaScript goes here
   //Test
+  $(".stream-2").toggle();
+  //console.log('Document is ready');
+  $("#switchStream").click(function() {
+    //console.log('Button clicked');
+    $(".stream-1").toggle();
+    $(".stream-2").toggle();
+  });
+
 });
--- static/site.js
+++ static/site.js
@@ -1,2 +1,2 @@
-jQuery(document).ready(function(e){});
+$(document).ready(function(t){t(".stream-2").toggle(),t("#switchStream").click(function(){t(".stream-1").toggle(),t(".stream-2").toggle()})});
 //# sourceMappingURL=site.js.map
